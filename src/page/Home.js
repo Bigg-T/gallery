c4f2f9bfe15ddc5687e4b14017077ac38fb3dda9
Thien Nguyen <thienn503@gmail.com>
2018-03-19 13:31:04 -0400

    started on display images on pages in grid

--- src/page/Home.js
+++ src/page/Home.js
@@ -3,8 +3,40 @@
  */
 import React, {Component} from 'react';
 import {Container} from 'semantic-ui-react';
+import ImageCard from '../component/ImageCard';
+import DisplayError from '../component/DisplayError';
+import Random from '../unsplash/Random';
+import * as util from '../unsplash/util';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isError : false,
+      code : 200,
+      msg : '',
+      results : [],
+    };
+    this.itemPerPage = 25;
+    this.itemPerRow = 3;
+    this.random = new Random(this.itemPerPage);
+  }
+
+  componentDidMount() {
+    let promise = this.random.getPromise();
+    promise
+    .then(res => {
+      const data = res.data;
+      this.setState({results : data});
+    })
+    .catch(res => {
+      res = res.response;
+      let code = res.status;
+      let msg = util.getErrorMsg(res.data);
+      this.setState({isError:true, msg:msg, code:code});
+    });
+  }
+
   render() {
     return "";
   }
