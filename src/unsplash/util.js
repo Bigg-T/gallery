0247303d033897b9630b67283733c39141a8388c
Thien Nguyen <thienn503@gmail.com>
2018-03-19 13:09:00 -0400

    tested basic util

--- src/unsplash/util.js
+++ src/unsplash/util.js
@@ -4,10 +4,21 @@
 
 import {UNSPLASH_BASE_URL, ACCESS_KEY} from './Constant';
 
+/**
+ * Generate a string.
+ *
+ * @returns {string} the client_id param for unsplash API auth
+ */
 export function clientID() {
   return 'client_id='+ ACCESS_KEY;
 }
 
+/**
+ * Generate a URL to user.
+ *
+ * @param username the username on unsplash
+ * @returns {string} link to user home page on unsplash
+ */
 export function userHref(username) {
   return UNSPLASH_BASE_URL + '@' + username;
 }
--- test/unsplash/util.test.js
+++ test/unsplash/util.test.js
@@ -4,3 +4,12 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import * as util from '../../src/unsplash/util';
+import {ACCESS_KEY, UNSPLASH_BASE_URL} from '../../src/unsplash/Constant';
+
+test('correctly generated client_id string', () => {
+  expect(util.clientID()).toBe('client_id='+ACCESS_KEY);
+});
+
+test('correctly generated user href (credit to user)', () => {
+  expect(util.userHref('test')).toBe(UNSPLASH_BASE_URL+'@test');
+});
