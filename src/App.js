6b47f388816875aa5a173823365b968ebc776ced
Thien Nguyen <thienn503@gmail.com>
2018-03-20 01:38:41 -0400

    Minor Fixed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import {Grid, Container} from 'semantic-ui-react';
 
 import MyRoute from './component/MyRoute';
+import MyNav from './component/MyNav';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Grid padded>
+          <Grid.Row>
+            <MyNav/>
+          </Grid.Row>
           <Grid.Row>
             <Container>
               <MyRoute />
--- test/page/Home.test.js
+++ test/page/Home.test.js
@@ -5,10 +5,10 @@ import React from 'react';
 import {shallow, mount} from 'enzyme';
 import Home from '../../src/page/Home';
 
-import jsdom from 'jsdom'
-const doc = jsdom.jsdom('<!doctype html><html><body></body></html>')
-global.document = doc
-global.window = doc.defaultView
+import jsdom from 'jsdom';
+const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.document = doc;
+global.window = doc.defaultView;
 
 test('Render Home without crashing', () => {
   let comp = shallow(<Home/>);
